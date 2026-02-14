7a67e8b4894586e15aeda80fc608a6677263c8c7
SujalPatil21 <sujalbpatil21@gmail.com>
2026-02-15 02:24:23 +0530

    [200~Backend Milestone: User, HealthRecord & JWT Authentication Implementation~

--- backend/src/main/java/com/aasha/backend/controller/UserController.java
+++ backend/src/main/java/com/aasha/backend/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.aasha.backend.controller;
 
+
 import com.aasha.backend.dto.UserRequest;
 import com.aasha.backend.dto.UserResponse;
 import com.aasha.backend.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
